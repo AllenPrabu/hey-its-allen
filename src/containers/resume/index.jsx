3b7fb078ae5ff6580cdbe5b01b49a6611c4cac87
AllenPrabu <allenthiagarajprabu@gmail.com>
2025-07-22 01:29:13 +0530

    Initial commit

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import PageHeaderContent from "../../components/pageheadercontent";
 import { BsInfoCircleFill } from "react-icons/bs";
+import { MdWork } from "react-icons/md";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
-import 'react-vertical-timeline-component/style.min.css'
+import "react-vertical-timeline-component/style.min.css";
+import PageHeaderContent from "../../components/pageheadercontent";
 import { data } from "./utils";
-import './styles.scss';
-import {MdWork} from 'react-icons/md';
+import "./styles.scss";
 
 const Resume = () => {
   return (
@@ -17,11 +17,14 @@ const Resume = () => {
         headerText="My Resume"
         icon={<BsInfoCircleFill size={40} />}
       />
+
       <div className="timeline">
+        {/* ===== Experience Section ===== */}
         <div className="timeline__experience">
           <h3 className="timeline__experience__header-text">Experience</h3>
+
           <VerticalTimeline
-            layout={"1-column"}
+            layout="1-column"
             lineColor="var(--yellow-theme-main-color)"
           >
             {data.experience.map((item, i) => (
@@ -29,73 +32,67 @@ const Resume = () => {
                 key={i}
                 className="timeline__experience__vertical-timeline-element"
                 contentStyle={{
-                  background: 'none',
-                  color : 'var(--yellow-theme-sub-text-color)',
-                  border : '1.5px solid var(--yellow-theme-main-color)'
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
                 }}
-                date="2020 - Present"
-                icon={<MdWork/>}
+                date={item.date || "2020 - Present"}
+                icon={<MdWork />}
                 iconStyle={{
-                  background : '#181818',
-                  color : 'var(--yellow-theme-main-color)',
-
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
                 }}
               >
                 <div className="vertical-timeline-element-title-wrapper">
-                  <h3>
-                    {item.title}
-                  </h3>
-
-                  <h4>
-                    {item.subTitle}
-                  </h4>
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
                 </div>
-                <p className="vertical-timeline-element-title-wrapper-description">{item.description} </p>
-
+                <p className="vertical-timeline-element-title-wrapper-description">
+                  {item.description}
+                </p>
               </VerticalTimelineElement>
             ))}
           </VerticalTimeline>
         </div>
+
+        {/* ===== Education Section ===== */}
         <div className="timeline__education">
           <h3 className="timeline__education__header-text">Education</h3>
+
           <VerticalTimeline
-            layout={"1-column"}
+            layout="1-column"
             lineColor="var(--yellow-theme-main-color)"
           >
-          {data.education.map((item, i) => (
+            {data.education.map((item, i) => (
               <VerticalTimelineElement
                 key={i}
-                className="timeline__experience__vertical-timeline-element"
+                className="timeline__education__vertical-timeline-element"
                 contentStyle={{
-                  background: 'none',
-                  color : 'var(--yellow-theme-sub-text-color)',
-                  border : '1.5px solid var(--yellow-theme-main-color)'
+                  background: "none",
+                  color: "var(--yellow-theme-sub-text-color)",
+                  border: "1.5px solid var(--yellow-theme-main-color)",
                 }}
-                date="2020 - Present"
-                icon={<MdWork/>}
+                date={item.date || "2020 - Present"}
+                icon={<MdWork />}
                 iconStyle={{
-                  background : '#181818',
-                  color : 'var(--yellow-theme-main-color)',
-
+                  background: "#181818",
+                  color: "var(--yellow-theme-main-color)",
                 }}
               >
                 <div className="vertical-timeline-element-title-wrapper">
-                  <h3>
-                    {item.title}
-                  </h3>
-
-                  <h4>
-                    {item.subTitle}
-                  </h4>
+                  <h3>{item.title}</h3>
+                  <h4>{item.subTitle}</h4>
                 </div>
-                <p className="vertical-timeline-element-title-wrapper-description">{item.description} </p>
-
+                <p className="vertical-timeline-element-title-wrapper-description">
+                  {item.description}
+                </p>
               </VerticalTimelineElement>
             ))}
-            </VerticalTimeline>
+          </VerticalTimeline>
         </div>
       </div>
     </section>
   );
 };
+
 export default Resume;
